c6a39cbaf22019df34a6b9beb52f830b12063b54
Taibe28 <taieb28box@gmail.com>
2026-02-16 21:03:46 +0100

    7 update

--- agent.py
+++ agent.py
@@ -1,60 +1,53 @@
-# agent.py
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.graph import StateGraph, MessagesState, START, END
+from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.graph.message import add_messages
 from langchain_core.messages import SystemMessage, AIMessage
-from typing import Annotated, TypedDict, List
+from typing import Annotated, TypedDict
 from tools import multiply
 import os
 
-
-# read the key
+# 
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
-tools = [multiply] # list of Tools
-
+# 
 SYSTEM_PROMPT = SystemMessage(
-    content=(
-       """
-	  انت مساعد ذكي ومتخصص اذا لم تكن لديك اداة لاستعمالها في الرد
-	  لاتقم بتخمين الاجابة وانما رد بالتالي "لا املك اداة لاستعمالها" """
-    )
+    content="""أنت مساعد ذكي ومتخصص. إذا لم تكن لديك أداة لاستعمالها في الرد، 
+    لا تقم بتخمين الإجابة وإنما رد بالتالي: "لا املك اداة لاستعمالها"."""
 )
 
-# 2. Configuraton
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+tools = [multiply]
+
+# 1. 
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash") 
 llm_with_tools = llm.bind_tools(tools)
 
-# 3. Agent Node
-def chatbot_node(state:State):
+# 2. 
+def chatbot_node(state: State):
+    full_messages = [SYSTEM_PROMPT] + state["messages"]
+    response = llm_with_tools.invoke(full_messages)
+    
+    if isinstance(response.content, list):
+        for item in response.content:
+            if isinstance(item, dict) and "text" in item:
+                response.content = item["text"]
+                break
+            elif isinstance(item, str):
+                response.content = item
+                break
 
-	messages = [SYSTEM_PROMPT] + state["messages"]
-    response = llm_with_tools.invoke(messages)
-	return {"messages":[response]}
+    return {"messages": [response]}
 
-# 4. Tool Node
-tool_node = ToolNode(tools)
-
+# 3
 builder = StateGraph(State)
-
-# Add Nodes
 builder.add_node("chatbot", chatbot_node)
-builder.add_node("tools", tool_node)
+builder.add_node("tools", ToolNode(tools))
 
-# Select first node
 builder.add_edge(START, "chatbot")
-
-# Add conditional node
-builder.add_conditional_edges(
-    "chatbot",
-    tools_condition, # it is function
-)
-# Add edge
+builder.add_conditional_edges("chatbot", tools_condition)
 builder.add_edge("tools", "chatbot")
 
-# 
 agent = builder.compile()
